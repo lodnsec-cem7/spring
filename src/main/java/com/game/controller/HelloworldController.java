bf52cf92a4551447bff3a9277617bd71bb26bf15
lodnsec-cem7 <dutanqian95971@126.com>
2018-01-24 18:03:52 +0800

    test

--- src/main/java/com/game/controller/HelloworldController.java
+++ src/main/java/com/game/controller/HelloworldController.java
@@ -1,8 +1,12 @@
 package com.game.controller;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,6 +14,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
 
 @Controller
 @RequestMapping("/helloworld") 
@@ -31,8 +39,19 @@ public class HelloworldController {
 		return "index";
 	}
 	@RequestMapping(value="/dologin",method=RequestMethod.POST)
-	public void dologin(HttpServletRequest request,HttpServletResponse response)throws IOException
+	public void dologin(HttpServletRequest request,HttpServletResponse response)throws IOException, ServletException
 	{
+
+	
+//		MultipartHttpServletRequest mRequest = (MultipartHttpServletRequest)request;
+		//System.out.println(mRequest.getParameter("username"));
+	
+		/*System.out.println(file);
+		System.out.println("123");
+
+		System.out.println(request.getPart("img"));
+		System.out.println("123");*/
+		
 		String username = request.getParameter("username").toString();
 		System.out.println(username);
 		System.out.println("123");
